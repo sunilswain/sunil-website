679e29cfc90fe117af871270b41c5c7a8cf57288
Sunil Swain <sunilswain5555@gmail.com>
2024-12-08 23:56:21 +0530

    v0.4: DotPattern BG - Photos

---
+++ utils/photos.ts
@@ -0,0 +1,88 @@
+// import Photo1 from "assets/photos/IMG_0620.jpeg"
+// import Photo2 from "assets/photos/IMG_0630.jpeg"
+// import Photo3 from "assets/photos/IMG_1691.jpeg"
+// import Photo4 from "assets/photos/IMG_2563.jpeg"
+// import Photo5 from "assets/photos/IMG_2571.jpeg"
+// import Photo6 from "assets/photos/IMG_2579.jpeg"
+// import Photo7 from "assets/photos/IMG_5398.jpeg"
+// import Photo8 from "assets/photos/IMG_5404.jpeg"
+// import Photo9 from "assets/photos/IMG_5472.jpeg"
+// import Photo10 from "assets/photos/IMG_5515.jpeg"
+// import Photo11 from "assets/photos/IMG_6162.jpeg"
+// import Photo12 from "assets/photos/IMG_6284.jpeg"
+// import Photo13 from "assets/photos/IMG_6288.jpeg"
+// import Photo14 from "assets/photos/IMG_6406.jpeg"
+// import Photo15 from "assets/photos/IMG_6422.jpeg"
+// import Photo16 from "assets/photos/IMG_6441.jpeg"
+// import Photo17 from "assets/photos/IMG_6826.jpeg"
+// import Photo18 from "assets/photos/IMG_6864.jpeg"
+// import Photo19 from "assets/photos/IMG_8434.jpeg"
+// import Photo20 from "assets/photos/IMG_8490.jpeg"
+// import Photo21 from "assets/photos/IMG_8513.jpeg"
+// import Photo22 from "assets/photos/IMG_8633.jpeg"
+// import Photo23 from "assets/photos/IMG_8798.jpeg"
+
+import Photo25 from "assets/photos/IMG-20241208-WA0005.jpg"
+import Photo24 from "assets/photos/IMG-20241208-WA0006.jpg"
+import Photo26 from "assets/photos/IMG-20241208-WA0007.jpg"
+import Photo27 from "assets/photos/IMG-20241208-WA0008.jpg"
+import Photo28 from "assets/photos/IMG-20241208-WA0009.jpg"
+import Photo29 from "assets/photos/IMG-20241208-WA0010.jpg"
+import Photo30 from "assets/photos/IMG-20241208-WA0011.jpg"
+import Photo31 from "assets/photos/IMG-20241208-WA0012.jpg"
+import Photo32 from "assets/photos/IMG-20241208-WA0013.jpg"
+import Photo33 from "assets/photos/IMG-20241208-WA0014.jpg"
+import Photo34 from "assets/photos/IMG-20241208-WA0015.jpg"
+import Photo35 from "assets/photos/IMG-20241208-WA0016.jpg"
+import Photo36 from "assets/photos/IMG-20241208-WA0017.jpg"
+import Photo37 from "assets/photos/IMG-20241208-WA0018.jpg"
+import Photo38 from "assets/photos/IMG-20241208-WA0019.jpg"
+import Photo39 from "assets/photos/IMG-20241208-WA0020.jpg"
+import Photo40 from "assets/photos/IMG-20241208-WA0021.jpg"
+import Photo41 from "assets/photos/WhatsApp Image 2024-12-08 at 22.17.32_addc3178.jpg"
+import Photo42 from "assets/photos/WhatsApp Image 2024-12-08 at 22.26.48_2f95a842.jpg"
+
+export const photos = [
+  // Photo1,
+  // Photo2,
+  // Photo3,
+  // Photo4,
+  // Photo5,
+  // Photo6,
+  // Photo7,
+  // Photo8,
+  // Photo9,
+  // Photo10,
+  // Photo11,
+  // Photo12,
+  // Photo13,
+  // Photo14,
+  // Photo15,
+  // Photo16,
+  // Photo17,
+  // Photo18,
+  // Photo19,
+  // Photo20,
+  // Photo21,
+  // Photo22,
+  // Photo23,
+  Photo24,
+  Photo25,
+  Photo26,
+  Photo27,
+  Photo28,
+  Photo29,
+  Photo30,
+  Photo31,
+  Photo32,
+  Photo33,
+  Photo34,
+  Photo35,
+  Photo36,
+  Photo37,
+  Photo38,
+  Photo39,
+  Photo40,
+  Photo41,
+  Photo42,
+]
