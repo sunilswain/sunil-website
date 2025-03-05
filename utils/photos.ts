357f3e1d622d11e12ccfb72d90b6f58248d32e0a
Sunil Swain <sunilswain5555@gmail.com>
2025-03-06 00:05:58 +0530

    Add Photos

--- utils/photos.ts
+++ utils/photos.ts
@@ -22,67 +22,59 @@ import Photo21 from "public/photos/IMG_8513.jpeg"
 import Photo22 from "public/photos/IMG_8633.jpeg"
 import Photo23 from "public/photos/IMG_8798.jpeg"
 
-// import Photo25 from "public/photos/IMG-20241208-WA0005.jpg"
-// import Photo24 from "public/photos/IMG-20241208-WA0006.jpg"
-// import Photo26 from "public/photos/IMG-20241208-WA0007.jpg"
-// import Photo27 from "public/photos/IMG-20241208-WA0008.jpg"
-// import Photo28 from "public/photos/IMG-20241208-WA0009.jpg"
-// import Photo29 from "public/photos/IMG-20241208-WA0010.jpg"
-// import Photo30 from "public/photos/IMG-20241208-WA0011.jpg"
-// import Photo31 from "public/photos/IMG-20241208-WA0012.jpg"
-// import Photo32 from "public/photos/IMG-20241208-WA0013.jpg"
-// import Photo33 from "public/photos/IMG-20241208-WA0014.jpg"
-// import Photo34 from "public/photos/IMG-20241208-WA0015.jpg"
-// import Photo35 from "public/photos/IMG-20241208-WA0016.jpg"
-// import Photo36 from "public/photos/IMG-20241208-WA0017.jpg"
-// import Photo37 from "public/photos/IMG-20241208-WA0018.jpg"
-// import Photo38 from "public/photos/IMG-20241208-WA0019.jpg"
-// import Photo39 from "public/photos/IMG-20241208-WA0020.jpg"
-// import Photo40 from "public/photos/IMG-20241208-WA0021.jpg"
-// import Photo41 from "public/photos/WhatsApp Image 2024-12-08 at 22.17.32_addc3178.jpg"
-// import Photo42 from "public/photos/WhatsApp Image 2024-12-08 at 22.26.48_2f95a842.jpg"
+import Photo24 from "public/blogPictures/IMG-20250303-WA0002.jpg"
+import Photo25 from "public/blogPictures/IMG-20250303-WA0003.jpg"
+import Photo26 from "public/blogPictures/IMG-20250303-WA0004.jpg"
+import Photo27 from "public/blogPictures/IMG-20250303-WA0005.jpg"
+import Photo28 from "public/blogPictures/IMG-20250303-WA0006.jpg"
+import Photo29 from "public/blogPictures/IMG-20250303-WA0007.jpg"
+import Photo30 from "public/blogPictures/IMG-20250303-WA0008.jpg"
+import Photo31 from "public/blogPictures/IMG-20250303-WA0009.jpg"
+import Photo32 from "public/blogPictures/IMG-20250303-WA0019.jpg"
+import Photo33 from "public/blogPictures/IMG-20250303-WA0020.jpg"
+// import Photo34 from "public/blogPictures/IMG-20250303-WA0021.jpg"
+import Photo35 from "public/blogPictures/IMG-20250303-WA0022.jpg"
+import Photo36 from "public/blogPictures/IMG-20250303-WA0023.jpg"
+import Photo37 from "public/blogPictures/IMG-20250303-WA0024.jpg"
+import Photo38 from "public/blogPictures/IMG-20250303-WA0025.jpg"
 
 export const photos = [
-  Photo1,
-  Photo2,
-  Photo3,
-  Photo4,
-  Photo5,
-  Photo6,
-  Photo7,
-  Photo8,
-  Photo9,
-  Photo10,
-  Photo11,
-  Photo12,
-  Photo13,
-  Photo14,
-  Photo15,
-  Photo16,
-  Photo17,
-  Photo18,
-  Photo19,
-  Photo20,
-  Photo21,
-  Photo22,
-  Photo23,
-  // Photo24,
-  // Photo25,
-  // Photo26,
-  // Photo27,
-  // Photo28,
-  // Photo29,
-  // Photo30,
-  // Photo31,
-  // Photo32,
-  // Photo33,
+  // Photo1,
+  // Photo2,
+  // Photo3,
+  // Photo4,
+  // Photo5,
+  // Photo6,
+  // Photo7,
+  // Photo8,
+  // Photo9,
+  // Photo10,
+  // Photo11,
+  // Photo12,
+  // Photo13,
+  // Photo14,
+  // Photo15,
+  // Photo16,
+  // Photo17,
+  // Photo18,
+  // Photo19,
+  // Photo20,
+  // Photo21,
+  // Photo22,
+  // Photo23,
+  Photo24,
+  Photo25,
+  Photo26,
+  Photo27,
+  Photo28,
+  Photo29,
+  Photo30,
+  Photo31,
+  Photo32,
+  Photo33,
   // Photo34,
-  // Photo35,
-  // Photo36,
-  // Photo37,
-  // Photo38,
-  // Photo39,
-  // Photo40,
-  // Photo41,
-  // Photo42,
+  Photo35,
+  Photo36,
+  Photo37,
+  Photo38,
 ]
